7c766aa85babacceb44606742e0d1312846f9ec6
adimis-ai <113940066+adimis-ai@users.noreply.github.com>
2023-01-15 20:03:33 +0530

    SvelteKit Extension Setup Completed

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,22 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "sveltekit-adapter-chrome-extension";
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter({
+			// default options are shown
+			pages: "build",
+			assets: "build",
+			fallback: null,
+			precompress: false,
+			manifest: "manifest.json",
+			emptyOutDir: true,
+		  }),	
+		appDir: "app",	
+	},
 };
 
 export default config;
