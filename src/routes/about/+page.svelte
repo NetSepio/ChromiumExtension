f587e99f7c96bb0fc075ae9cdf851f9ebb1cb9a3
Iswar <rick731202@gmail.com>
2023-01-16 10:23:33 +0530

    Configured the store

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,26 +1,29 @@
+<script>
+	import {walletAddress, jwtToken, mnemonicPhase, onboardingStepsLeft} from "../../store/store"
+</script>
+
 <svelte:head>
 	<title>About</title>
 	<meta name="description" content="About this app" />
 </svelte:head>
 
-<div class="text-column">
-	<h1>About this app</h1>
-
-	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
-	</p>
-
-	<pre>npm create svelte@latest</pre>
-
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
-
-	<p>
-		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
-		using it with JavaScript disabled!
-	</p>
+<div>
+	the wallet address is {$walletAddress}
+	<button on:click={()=>walletAddress.setWalletAddress("new wallet address")}>Change walletAddress</button>
 </div>
+<div>
+	the wallet address is {$jwtToken}
+	<button on:click={()=>jwtToken.setJwtToken("new jwt token")}>Change jwtToken</button>
+</div>
+<div>
+	the wallet address is {$mnemonicPhase}
+	<button on:click={()=>mnemonicPhase.setMnemonicPhase("new mnemonic phase")}>Change mnemonic</button>
+</div>
+<div>
+	the onboardingStepsLeft is {$onboardingStepsLeft}
+	<button on:click={()=>onboardingStepsLeft.setOnboardingStepsLeft(100)}>Change onboardingStepsLeft</button>
+	<button on:click={()=>onboardingStepsLeft.increaseOnboardingStepsLeft()}>increase onboardingStepsLeft</button>
+	<button on:click={()=>onboardingStepsLeft.decreaseOnboardingStepsLeft()}>decrease onboardingStepsLeft</button>
+</div>
+
+
