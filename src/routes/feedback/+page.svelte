482c1a3e3650f8ce13f5a0d64a8190a8f50d8548
Iswar <rick731202@gmail.com>
2023-01-24 23:20:02 +0530

    Submitting feedback is working

--- src/routes/feedback/+page.svelte
+++ src/routes/feedback/+page.svelte
@@ -1,26 +1,112 @@
-<script>
+<script lang="ts">
 	import Header from '$lib/components/Header.svelte';
+	import { PUBLIC_GATEWAY_URL } from '$env/static/public';
+	import { jwtToken } from '$lib/store/store';
+
+	let rating: number;
+	let feedbackText: string;
+	let error: string;
+	let successful: boolean;
+
+	const selectRatingHandler = (e: any) => {
+		rating = e.target.value;
+	};
+
+	const submitReviewHandler = async () => {
+		try {
+			const response = await fetch(`${PUBLIC_GATEWAY_URL}/feedback`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: $jwtToken
+				},
+				body: JSON.stringify({
+					feedback: feedbackText,
+					rating: Number(rating)
+				})
+			});
+
+			if (response.status === 401) {
+				error = 'Setup wallet and sing up first';
+			}
+			if (response.status === 200) {
+				successful = true;
+			}
+		} catch (err) {
+			error = err.message;
+		}
+	};
 </script>
 
 <div class="p-5">
 	<Header />
 	<br />
-	<div>
-		<h1 class="text-3xl text-left mt-5 mb-5">Rate Us</h1>
-		<div class="rating">
-			<input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
-			<input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" checked />
-			<input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
-			<input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
-			<input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+	{#if successful}
+		<div class="text-3xl text-center">Your feedback means a lot to us, so thank you :)</div>
+	{:else}
+		<div>
+			<h1 class="text-3xl text-left mt-5 mb-5">Rate Us</h1>
+			<div class="rating">
+				<input
+					type="radio"
+					name="rating-2"
+					class="mask mask-star-2 bg-orange-400"
+					on:click={(e) => {
+						selectRatingHandler(e);
+					}}
+					checked
+					value="1"
+				/>
+				<input
+					type="radio"
+					name="rating-2"
+					class="mask mask-star-2 bg-orange-400"
+					on:click={(e) => {
+						selectRatingHandler(e);
+					}}
+					value="2"
+				/>
+				<input
+					type="radio"
+					name="rating-2"
+					class="mask mask-star-2 bg-orange-400"
+					on:click={(e) => {
+						selectRatingHandler(e);
+					}}
+					value="3"
+				/>
+				<input
+					type="radio"
+					name="rating-2"
+					class="mask mask-star-2 bg-orange-400"
+					on:click={(e) => {
+						selectRatingHandler(e);
+					}}
+					value="4"
+				/>
+				<input
+					type="radio"
+					name="rating-2"
+					class="mask mask-star-2 bg-orange-400"
+					on:click={(e) => {
+						selectRatingHandler(e);
+					}}
+					value="5"
+				/>
+			</div>
 		</div>
-	</div>
 
-	<div>
-		<h1 class="text-5xl text-left mt-10 mb-5">Write to Us!</h1>
-	</div>
+		<div>
+			<h1 class="text-5xl text-left mt-10 mb-5" class:text-red-500={error}>
+				{error ?? 'Write to Us!'}
+			</h1>
+		</div>
 
-	<textarea class="textarea textarea-bordered w-full h-44 dark:text-white dark:bg-gray-900 border border-zinc-600" placeholder="Write Here" />
-
-	<button class="btn mt-5">Submit</button>
+		<textarea
+			class="textarea textarea-bordered w-full h-44 dark:text-white dark:bg-gray-900 border border-zinc-600"
+			placeholder="Write Here"
+			bind:value={feedbackText}
+		/>
+		<button class="btn mt-5" on:click={submitReviewHandler}>Submit</button>
+	{/if}
 </div>
