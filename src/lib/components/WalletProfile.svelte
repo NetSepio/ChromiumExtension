ea17992d513e3c1dadcb7bdcd95f19a9db60da5b
Iswar <rick731202@gmail.com>
2023-01-28 16:13:37 +0530

    Added some integrations in WalletProfile

--- src/lib/components/WalletProfile.svelte
+++ src/lib/components/WalletProfile.svelte
@@ -1,23 +1,45 @@
-<script>
+<script lang="ts">
 	import Icon from 'svelte-icons-pack/Icon.svelte';
 	import AiFillCopy from 'svelte-icons-pack/ai/AiFillCopy';
 	import RiFinanceCoinsFill from 'svelte-icons-pack/ri/RiFinanceCoinsFill';
 	import AiOutlineSend from 'svelte-icons-pack/ai/AiOutlineSend';
 	import RiFinanceExchangeFill from 'svelte-icons-pack/ri/RiFinanceExchangeFill';
-
 	import MaticImg from '$lib/images/matic-token.png';
+	import { walletAddress } from '$lib/store/store';
+	import { onMount } from 'svelte';
+	import { ethers } from 'ethers';
 
 	let truncatedAddress = '';
-	let walletBalance = 0;
-	let walletAddress = '';
+	let walletBalance = '...';
+	let userWalletAddress = '';
+
+	walletAddress.subscribe((value) => (userWalletAddress = value));
+
 	const handleCopyClick = () => {
-		navigator.clipboard.writeText(walletAddress);
+		navigator.clipboard.writeText(userWalletAddress);
 	};
+
+	const getWalletBalance = async () => {
+		const provider = new ethers.providers.JsonRpcProvider(
+			'https://polygon-mumbai.g.alchemy.com/v2/QuHFh_kiJbakSBUi8Js2Jtvaz6WfE-if'
+		);
+		let balanceInWei = await provider.getBalance(userWalletAddress);
+
+		walletBalance = ethers.utils.formatEther(balanceInWei);
+		console.log(walletBalance);
+	};
+
+	onMount(() => {
+		truncatedAddress = `${userWalletAddress.substring(0, 5)}...${userWalletAddress.substring(
+			userWalletAddress.length - 4
+		)}`;
+		getWalletBalance();
+	});
 </script>
 
 <div class="flex flex-col items-center">
 	<div class="flex items-center mb-4">
-		<h1 class="font-bold text-black text-lg">{truncatedAddress}</h1>
+		<h1 class="font-bold text-black dark:text-white text-lg">{truncatedAddress}</h1>
 		<button
 			class="ml-1 px-4 py-2 rounded-lg bg-zinc-200 text-white w-auto h-auto content-around"
 			on:click={handleCopyClick}
@@ -29,7 +51,7 @@
 	<div class="flex flex-col mb-4">
 		<img src={MaticImg} alt="MATIC token" class="h-16 w-16 flex items-center mx-28 mb-4" />
 		<div class="flex justify-center">
-			<span class="text-4xl text-center">{walletBalance} MATIC</span>
+			<span class="text-4xl text-center">{walletBalance} ETH</span>
 		</div>
 	</div>
 
