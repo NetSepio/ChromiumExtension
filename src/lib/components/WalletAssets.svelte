31e0ae91e8059c4238e017da335d68fa908cdbfc
Iswar <rick731202@gmail.com>
2023-02-04 21:45:22 +0530

    wallet assets functionality added 📜

--- src/lib/components/WalletAssets.svelte
+++ src/lib/components/WalletAssets.svelte
@@ -1,6 +1,24 @@
 <script lang="ts">
+	import { walletAddress } from '$lib/store/store';
+	import { PUBLIC_ETHERSCAN_API_KEY } from '$env/static/public';
+	import { onMount } from 'svelte';
+
 	let assets: any = [];
 	let showModal = false;
+	async function getTokenAddresses(address: string) {
+		const url = `https://api.etherscan.io/api?module=account&action=tokentx&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=${PUBLIC_ETHERSCAN_API_KEY}`;
+
+		const response = await fetch(url);
+		const json = await response.json();
+		const tokenAddresses = Array.from(new Set(json.result.map((tx: any) => tx.contractAddress)));
+
+		console.log('Token addresses:', tokenAddresses);
+		assets = json.result;
+	}
+
+	onMount(() => {
+		getTokenAddresses($walletAddress as string);
+	});
 </script>
 
 <div class="flex flex-col mb-4">
@@ -8,12 +26,13 @@
 
 	{#if assets.length === 0}
 		<div>
-			<button
+			<!-- <button
 				class="modal-button btn px-4 py-2 rounded-md shadow-lg bg-zinc-700 text-white dark:bg-gray-900 dark:text-white"
 				on:click={() => (showModal = true)}
 			>
 				Import Token
-			</button>
+			</button> -->
+			<h1 class="text-lg p-3">No Assets Found</h1>
 			<div class="modal modal-bottom sm:modal-middle" class:modal-open={showModal}>
 				<div class="modal-box dark:bg-gray-900 dark:text-white">
 					<button
